5cd62f1a9009c5735807a6920961bfb57c116c38
drkbyte <drkbyte@users.noreply.github.com>
2015-01-11 09:44:26 -0700

    Update edgar.py

--- edgar.py
+++ edgar.py
@@ -5,25 +5,33 @@ import os
 
 link_list = [ ]
 
-base_url = "http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001383701&type=&datea=20140930&owner=exclude&output=xml&count=10"
+base_url = "http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=CNAT&type=10-Q&datea=20140930&owner=exclude&output=xml&count=10"
 r = requests.get(base_url)
 data = r.text
 soup = BeautifulSoup(data)
-
+ 
 for link in soup.find_all('filinghref'):
-	#print link
 	l1 = str(link)
-	#print type(l1)
 	l2 = l1.replace('<filinghref>','')
-	#print l2
-	l3 = l2.replace('</filinghref>','')
-	link_list.append(l3)
+	link_list = l2.replace('</filinghref>','')
 
-print link_list
-base_url = link_list[0]
-r = requests.get(base_url)
+r = requests.get(link_list)
 data = r.text
 soup = BeautifulSoup(data)
-print soup
-#l2 = l1.strip('<filinghref>')
-#l3 = l2.strip('</filinghref>')
+
+listr = [ ]
+
+for link in soup.find_all('a'):
+	listr.append(link)
+
+for l in listr:
+	print l
+
+listr[9]
+new_link = 	listr[9]
+n2 = str(new_link)
+n3 = n2.replace('<a href="','http://www.sec.gov')
+n4 = n3.partition('"')
+n5 = n4[0]
+source_doc = str(n5)
+print source_doc
